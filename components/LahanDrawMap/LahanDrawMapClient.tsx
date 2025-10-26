8a7cf23dcfae3e5d2f5e74e5f48b5ba7521eae52
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2025-10-26 19:02:26 +0800

    feat: lahan page update

--- components/LahanDrawMap/LahanDrawMapClient.tsx
+++ components/LahanDrawMap/LahanDrawMapClient.tsx
@@ -182,8 +182,9 @@ export function LahanDrawMapClient({
         className="z-0"
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+          maxZoom={19}
         />
         <GeolocationButton />
         <DrawControl
--- components/LahanMap/LahanMapClient.tsx
+++ components/LahanMap/LahanMapClient.tsx
@@ -46,8 +46,9 @@ export function LahanMapClient({
         className="z-0"
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+          maxZoom={19}
         />
 
         {lahanList.map((lahan) => {
